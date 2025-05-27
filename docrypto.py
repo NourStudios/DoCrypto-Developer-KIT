8ea3b9746a96d77b49ebba21ec76ef52966290b2
max <93056030+NourStudios@users.noreply.github.com>
2025-05-27 16:17:52 +0300

    Add files via upload

--- docrypto.py
+++ docrypto.py
@@ -49,29 +49,6 @@ def add_supply(crypto_id, amount_add_supply):
  finally:
      driver.quit()
 
-def run_algorithm(crypto_id, amount_algorithm, owner_pct):
- # URL with query string
- base_url = "https://docryptonet.infinityfreeapp.com/app/algorithm.php"
- url = f"{base_url}?crypto={crypto_id}&amount={amount_algorithm}&owner_pct={owner_pct}"
- # Chrome options
- options = Options()
- options.add_argument("--headless")
- options.add_argument("--disable-gpu")
- options.add_argument("--log-level=3")  # Suppress console logs
-
- # Suppress "DevTools listening" by redirecting service logs
- service = Service(log_path=os.devnull)
-
- # Start WebDriver
- driver = webdriver.Chrome(service=service, options=options)
-
- try:
-     driver.get(url)
-     plain_output = driver.find_element("tag name", "body").text
-     print(plain_output)
- finally:
-     driver.quit()
-
 def check_balance(crypto_id):
  # URL with query string
  base_url = "https://docryptonet.infinityfreeapp.com/app/balance_checker.php"
